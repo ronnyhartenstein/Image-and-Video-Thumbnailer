e72b2d44d6af8e3509f40da4f626f32fa35236a7
Ronny Hartenstein <ronny@rh-flow.de>
2017-01-04 15:52:16 +0100

    Logfiles

--- _helper.php
+++ _helper.php
@@ -12,22 +12,34 @@ function notify($message, $subtitle='') {
   exec($cmd);
 }
 
+$logfile_name = dirname($argv[0]).'/'.basename($argv[0],'.php').'_lastrun.log';
+if (file_exists($logfile_name)) {
+  unlink($logfile_name);
+}
+function logfile($level, $msg) {
+  global $logfile_name;
+  file_put_contents($logfile_name, date('Y-m-d H:i:s')." ${level} ${msg}");
+}
+
 function log_error($msg) {
   // echo "ERROR: $msg\n";
   notify($msg, "FEHLER");
   trigger_error($msg, E_USER_ERROR);
+  logfile("ERROR", $msg);
 }
 
 function log_warning($msg) {
   // echo "WARN: $msg\n";
   notify($msg, "WARNUNG");
   trigger_error($msg, E_USER_WARNING);
+  logfile("WARN", $msg);
 }
 
 function log_debug($msg) {
   global $debug;
   if ($debug) {
     echo "DEBUG: $msg\n";
+    logfile("DEBUG", $msg);
   }
 //  trigger_error($msg, E_USER_NOTICE);
 }
@@ -36,6 +48,7 @@ function log_info($msg) {
   if (ini_get('error_reporting') & E_NOTICE) {
     notify($msg);
     echo "INFO: $msg\n";
+    logfile("INFO", $msg);
   }
 //  trigger_error($msg, E_USER_NOTICE);
 }
